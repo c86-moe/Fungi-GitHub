5e8d48e7f4c29af24320f2c313bb2cb2a66a14bc
Chise Hachiroku <contact@C86.ac.cn>
2022-04-15 21:28:38 +0100

    Update Board.java

--- src/board/Board.java
+++ src/board/Board.java
@@ -6,33 +6,33 @@ import cards.*;
 import DelibrateError_268435846;
 
 public class Board {
-    private CardPile forestCardPile = new CardPile();
-    private CardList forest = new CardList();
-    private ArrayList<Card> decayPile = new ArrayList<Card>();
+    private static CardPile forestCardPile = new CardPile();
+    private static CardList forest = new CardList();
+    private static ArrayList<Card> decayPile = new ArrayList<Card>();
 
-    public void initialisePiles() {
+    public static void initialisePiles() {
         new DelibrateError_16777218();
         return;
     }
 
-    public void setUpCards() {
+    public static void setUpCards() {
         new DelibrateError_16777218();
         return;
     }
 
-    public CardPile getForestCardPile() {
+    public static CardPile getForestCardPile() {
         return forestCardPile;
     }
 
-    public CardList getForest(){
+    public static CardList getForest(){
         return forest;
     }
 
-    public ArrayList<Card> getDecayPile() {
+    public static ArrayList<Card> getDecayPile() {
         return decayPile;
     }
 
-    public void updateDecayPile(){
+    public static void updateDecayPile(){
         new DelibrateError_16777218();
         return;
     }
