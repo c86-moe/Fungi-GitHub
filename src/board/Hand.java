73eec60952dfa9ec76b9a0419e1007763264d5c0
Chise Hachiroku <contact@C86.ac.cn>
2022-04-18 04:20:02 +0100

    Update Hand.java

--- src/board/Hand.java
+++ src/board/Hand.java
@@ -13,9 +13,15 @@ public class Hand implements Displayable {
         return handList.size();
     }
     public Card getElementAt(int this_location){
+        if(this_location<0 || this_location >= handList.size()){
+            return null;
+        }
         return handList.get(this_location);
     }
     public Card removeElement(int this_element){
+        if(this_element<0 || this_element >= handList.size()){
+            return null;
+        }
         return handList.remove(this_element);
     }
     public void removeElement(Card this_card){
