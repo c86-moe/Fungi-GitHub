0eb749b74760d2734c96e6344480a9ad0eedabe5
Chise Hachiroku <contact@C86.ac.cn>
2022-04-19 13:56:42 +0100

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,3 @@
 javac -cp .:junit-platform-console-standalone.jar --source-path ./src/ ./test/cards/*.java ./test/board/*.java -d ./bin/test/
-java -jar junit-platform-console-standalone.jar  --class-path ./bin/test/ --scan-class-path --fail-if-no-tests
+java -jar junit-platform-console-standalone.jar  --class-path ./bin/test/cards/ --scan-class-path --fail-if-no-tests
+java -jar junit-platform-console-standalone.jar  --class-path ./bin/test/board/ --scan-class-path --fail-if-no-tests
